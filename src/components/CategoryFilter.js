fa5dee73b91f36fcce42e1cb8328fb96628564c7
miriam590 <miriam.yego@student.moringaschool.com>
2025-01-19 16:18:38 +0300

    completed

--- src/components/CategoryFilter.js
+++ src/components/CategoryFilter.js
@@ -1,10 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 
-function CategoryFilter() {
+function CategoryFilter({ categories, onCategorySelect }) {
+  const [selectedCategory, setSelectedCategory] = useState("All");
+
+  function handleCategoryClick(category) {
+    setSelectedCategory(category);
+    onCategorySelect(category);
+  }
+
   return (
     <div className="categories">
       <h5>Category filters</h5>
-      {/* render <button> elements for each category here */}
+      {categories.map((category) => (
+        <button
+          key={category}
+          className={selectedCategory === category ? "selected" : ""}
+          onClick={() => handleCategoryClick(category)}
+        >
+          {category}
+        </button>
+      ))}
     </div>
   );
 }
